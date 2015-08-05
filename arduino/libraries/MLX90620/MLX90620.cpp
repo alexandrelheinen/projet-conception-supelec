c9834f24bd3825a88e020f5463524e60c21a953a
Alexandre Loeblein Heinen <alexandrelheinen@gmail.com>
2015-08-05 23:32:04 +0200

    MLX90620 changed to Object Oriented structure. Class MLX90620 was created and all the functions are incorporated inside this class.
    The loop function of .ino file is just an execution of MLX90620.loop() function.

--- arduino/libraries/MLX90620/MLX90620.cpp
+++ arduino/libraries/MLX90620/MLX90620.cpp
@@ -13,18 +13,56 @@
 
 #include "MLX90620.h"
 
-extern float calculate_TA (float k_t1, float k_t2, int v_th, int PTAT)
+MLX90620::MLX90620(int f)
 {
-    float ta;
-    ta = (-k_t1 + sqrt(k_t1*k_t1 - 4.0*k_t2*(v_th - PTAT)))/(2.0*k_t2) + 25.0; 	//it's much more simple now, isn't it? :)
-    return ta;
+  // data initialization
+  this->frequency = f;
+  read_EEPROM_MLX90620();
+  varInitialization();
+  config_MLX90620_Hz(this->frequency);
+  this->counter = 0;
 }
 
-extern void calculate_TO(int a_cp, int b_cp, int a_ij[64], int b_ij[64], float alpha_ij[64], float emissivity, float ta, int tgc, int CPIX, int b_i_scale, int IRDATA[64], float temperatures[64])
+MLX90620::~MLX90620()
+{
+
+}
+
+void MLX90620::loop()
+{
+  delay(1000.0/this->frequency); // a little delay
+
+  // **************** IMAGE ACQUISITION **************************************
+   if(counter == 0)
+   {		                //TA refresh is slower than the pixel readings, I'll read the values and computate them not every loop.
+     readPTAT();        // PTAT stands for pro Proportional To Absolute Temperature
+     calculateTA();     // ambient's temperature
+     checkConfigReg();  // reconfigure if it's needed
+   }
+
+   counter++;
+   if(counter >= 16)
+      counter = 0;
+
+   // read and calculate
+   readIRDATA(sensorData); // gets the lecture from the sensor
+   readCPIX();
+   calculateTO();
+
+   // DATA TRANSMITION
+   transmitTemperatures();        // after we transmit all the image
+}
+
+void MLX90620::calculateTA ()
+{
+    t_amb = (-k_t1 + sqrt(k_t1*k_t1 - 4.0*k_t2*(v_th - PTAT)))/(2.0*k_t2) + 25.0;
+}
+
+void MLX90620::calculateTO()
 {
     float temp = 0;                                                             // auxiliare
     float v_comp = CPIX - (a_cp +  b_cp/pow(2, b_i_scale)*(ta - 25.0));         // temperature gradient pixel
-    for (int i=0; i<64; i++)
+    for (int i = 0; i < 64; i++)
     {
         temp = IRDATA[i] - (a_ij[i] +  b_ij[i]/pow(2, b_i_scale)*(ta - 25.0));  // offset compensation
         temp = temp - tgc/32.0*v_comp;                                          // temperature gradient compensation
@@ -33,44 +71,42 @@ extern void calculate_TO(int a_cp, int b_cp, int a_ij[64], int b_ij[64], float a
     }
 }
 
-extern void check_Config_Reg_MLX90620(int freq)
+void MLX90620::checkConfigReg()
 {
-    int CFG = read_Config_Reg_MLX90620();
+    int CFG = readConfigReg();
     byte CFG_MSB = (byte) (CFG >> 8);
     if ((!CFG_MSB & 0x04) == 0x04)
-    {
-        config_MLX90620_Hz(freq);
-    }
+        config(this->frequency);
 }
 
-extern void config_MLX90620_Hz(int Hz)
+void MLX90620::config(int Hz)
 {
     byte Hz_LSB;
     switch(Hz)
     {
-    case 0:
-        Hz_LSB = B00001111;
-        break;
-    case 1:
-        Hz_LSB = B00001110;
-        break;
-    case 2:
-        Hz_LSB = B00001101;
-        break;
-    case 4:
-        Hz_LSB = B00001100;
-        break;
-    case 8:
-        Hz_LSB = B00001011;
-        break;
-    case 16:
-        Hz_LSB = B00001010;
-        break;
-    case 32:
-        Hz_LSB = B00001001;
-        break;
-    default:
-        Hz_LSB = B00001110;
+      case 0:
+          Hz_LSB = B00001111;
+          break;
+      case 1:
+          Hz_LSB = B00001110;
+          break;
+      case 2:
+          Hz_LSB = B00001101;
+          break;
+      case 4:
+          Hz_LSB = B00001100;
+          break;
+      case 8:
+          Hz_LSB = B00001011;
+          break;
+      case 16:
+          Hz_LSB = B00001010;
+          break;
+      case 32:
+          Hz_LSB = B00001001;
+          break;
+      default:
+          Hz_LSB = B00001110;
     }
     i2c_start_wait(0xC0);
     i2c_write(0x03);
@@ -81,7 +117,7 @@ extern void config_MLX90620_Hz(int Hz)
     i2c_stop();
 }
 
-extern int read_Config_Reg_MLX90620()
+void MLX90620::readConfigReg()
 {
     byte CFG_MSB, CFG_LSB;
     i2c_start_wait(0xC0);
@@ -93,11 +129,10 @@ extern int read_Config_Reg_MLX90620()
     CFG_LSB = i2c_readAck();
     CFG_MSB = i2c_readAck();
     i2c_stop();
-    int CFG = (CFG_MSB << 8) + CFG_LSB;
-    return CFG;
+    this->CFG = (CFG_MSB << 8) + CFG_LSB;
 }
 
-extern int read_CPIX_Reg_MLX90620()
+void MLX90620::readCPIX()
 {
     byte CPIX_LSB, CPIX_MSB;
     i2c_start_wait(0xC0);
@@ -109,24 +144,33 @@ extern int read_CPIX_Reg_MLX90620()
     CPIX_LSB = i2c_readAck();
     CPIX_MSB = i2c_readAck();
     i2c_stop();
-    int CPIX = (CPIX_MSB << 8) + CPIX_LSB;
-    return CPIX;
+    this->CPIX = (CPIX_MSB << 8) + CPIX_LSB;
 }
 
-extern void read_EEPROM_MLX90620(byte* EEPROM_DATA)
+byte MLX90620::getEEPROM(int ind)
+{
+  return EEPROM_DATA[ind];
+}
+
+void MLX90620::readEEPROM()
 {
     i2c_start_wait(0xA0);
     i2c_write(0x00);
     i2c_rep_start(0xA1);
-    for(int i=0; i<=255; i++)
+    for(unsigned int i = 0; i < 256; i++)
     {
-        EEPROM_DATA[i] = i2c_readAck();
+      this->EEPROM_DATA[i] = i2c_readAck();
     }
     i2c_stop();
-    write_trimming_value(EEPROM_DATA[0xF7]);
+    writeTrimmingValue(this->EEPROM_DATA[0xF7]);
 }
 
-extern void read_IR(int IRDATA[64])
+int getIRDATA(int ind)
+{
+  return this->IRDATA[ind];
+}
+
+void MLX90620::readIR()
 {
     byte PIX_LSB;
     byte PIX_MSB;
@@ -136,16 +180,16 @@ extern void read_IR(int IRDATA[64])
     i2c_write(0x01);
     i2c_write(0x40);
     i2c_rep_start(0xC1);
-    for(int i=0; i<=63; i++)
+    for(int i = 0; i < 64; i++)
     {
         PIX_LSB = i2c_readAck();
         PIX_MSB = i2c_readAck();
-        IRDATA[i] = (PIX_MSB << 8) + PIX_LSB;
+        this->IRDATA[i] = (PIX_MSB << 8) + PIX_LSB;
     }
     i2c_stop();
 }
 
-extern unsigned int read_PTAT_Reg_MLX90620 ()
+void MLX90620::readPTAT()
 {
     byte PTAT_LSB, PTAT_MSB;
     i2c_start_wait(0xC0);
@@ -157,19 +201,16 @@ extern unsigned int read_PTAT_Reg_MLX90620 ()
     PTAT_LSB = i2c_readAck();
     PTAT_MSB = i2c_readAck();
     i2c_stop();
-    unsigned int PTAT = ((unsigned int)PTAT_MSB << 8) + PTAT_LSB;
-    return PTAT;
+    this->PTAT = ((unsigned int)PTAT_MSB << 8) + PTAT_LSB;
 }
 
-extern void transmit_temperatures (float* temperatures)
+void transmitTemperatures()
 {
     for(int i=0; i<=63; i++)
-    {
-        Serial.println(temperatures[i]);
-    }
+        Serial.println(this->temperatures[i]);
 }
 
-void write_trimming_value(byte val)
+void writeTrimmingValue(byte val)
 {
     i2c_start_wait(0xC0);
     i2c_write(0x04);
@@ -180,39 +221,53 @@ void write_trimming_value(byte val)
     i2c_stop();
 }
 
-/** THIS FUNCTION IS JUST AN EXAMPLE OF HOW TO INITIALIZE THE SENSOR'S VARIABLES
-
 void varInitialization(){
- v_th = (EEPROM_DATA[219] <<8) + EEPROM_DATA[218];
- k_t1 = ((EEPROM_DATA[221] <<8) + EEPROM_DATA[220])/1024.0;
- k_t2 =((EEPROM_DATA[223] <<8) + EEPROM_DATA[222])/1048576.0;
+  // those formulas can be found on page 14 of the datasheet
+   v_th = (EEPROM_DATA[0xDB] <<8) + EEPROM_DATA[0xDA];
+   k_t1 = ((EEPROM_DATA[0xDD] <<8) + EEPROM_DATA[0xDC])/1024.0;
+   k_t2 =((EEPROM_DATA[0xDF] <<8) + EEPROM_DATA[0xDE])/1048576.0;
 
- a_cp = EEPROM_DATA[212];
- if(a_cp > 127){
-   a_cp = a_cp - 256;
- }
- b_cp = EEPROM_DATA[213];
- if(b_cp > 127){
-   b_cp = b_cp - 256;
- }
- tgc = EEPROM_DATA[216];
- if(tgc > 127){
-   tgc = tgc - 256;
- }
-
- b_i_scale = EEPROM_DATA[217];
-
- emissivity = (((unsigned int)EEPROM_DATA[229] << 8) + EEPROM_DATA[228])/32768.0;
-
- for(int i=0;i<=63;i++){
-   a_ij[i] = EEPROM_DATA[i];
-   if(a_ij[i] > 127){
-     a_ij[i] = a_ij[i] - 256;
+    // page 17
+   a_cp = EEPROM_DATA[0xD4];
+   if(a_cp > 127){
+     a_cp = a_cp - 256;
    }
-   b_ij[i] = EEPROM_DATA[64+i];
-   if(b_ij[i] > 127){
-     b_ij[i] = b_ij[i] - 256;
+
+   b_cp = EEPROM_DATA[0xD5];
+   if(b_cp > 127){
+     b_cp = b_cp - 256;
    }
- }
+
+   tgc = EEPROM_DATA[0xD8];
+   if(tgc > 127){
+     tgc = tgc - 256;
+   }
+
+   b_i_scale = EEPROM_DATA[0xD9];
+
+   emissivity = (((unsigned int)EEPROM_DATA[0xE5] << 8) + EEPROM_DATA[0xE4])/32768.0;
+
+   for(int i=0;i < 64; i++){
+     a_ij[i] = EEPROM_DATA[i]; // les valeurs des As commencent en 0x00
+     if(a_ij[i] > 127){
+       a_ij[i] = a_ij[i] - 256; // stored as 2's complement!
+     }
+     b_ij[i] = EEPROM_DATA[0x40+i]; // et celles des Bs, en 0x40
+     if(b_ij[i] > 127){
+       b_ij[i] = b_ij[i] - 256;  // stored as 2's complement!
+     }
+   }
+
+   // calculation of alphas
+   // the datasheet specifies where each value can be found on the EEPROM
+   unsigned int alpha0_H, alpha0_L, alpha0_S, delta_alpha_S, delta_alpha_ij; // we just name those variables to make the code cleaner
+   alpha0_L = EEPROM_DATA[0xE0];
+   alpha0_H = EEPROM_DATA[0xE1];
+   alpha0_S = EEPROM_DATA[0xE2];
+   delta_alpha_S = EEPROM_DATA[0xE3]; // because we could use directly the EEPROM_DATA
+   int c;
+   for (c=0; c < 64; c++) {
+     delta_alpha_ij = EEPROM_DATA[0x80 + c];
+     alpha_ij[c] = (256.0*alpha0_H + alpha0_L)/(pow(2, alpha0_S)) + delta_alpha_ij/(pow(2, delta_alpha_S)); // we can found this formula on datasheet's page 17
+   }
 }
-************************************************************************************/
--- arduino/libraries/MLX90620/MLX90620.h
+++ arduino/libraries/MLX90620/MLX90620.h
@@ -1,14 +1,13 @@
 #ifndef MLX90620_H_
 #define MLX90620_H_
-#endif // MLX90620_H_
 
 /****************************************************************************
  * Title: Arduino based library to MLX90620 sensor
  * This library was based on IlBaboomba's code to MLX90620 sensor. The original code is disponible in http://forum.arduino.cc/index.php?topic=126244.0
  * Any other iformation like formulas or definition can be found on the sensor's datasheet which is disponible in http://www.melexis.com/Infrared-Thermometer-Sensors/Infrared-Thermometer-Sensors/MLX90620-776.aspx
  * Authors: Alexandre Loeblein Heinen & Clyvian Ribeiro Borges
- * CentraleSupélec :: Projet de Conception
- * Binôme A6.66 :: 2014/2015
+ * CentraleSupÃ©lec :: Projet de Conception
+ * BinÃ´me A6.66 :: 2014/2015
  * Date: May 22, 2015
  ****************************************************************************/
 
@@ -17,70 +16,55 @@
 #include <i2cmaster.h>
 #include <Arduino.h>
 
-/**
- \brief Calculate the ambient temperatured based on the input data. All the formulas can be found on the datasheet. See pages 14 and 15 of the datasheet.
- \param  The calculation required parameters.
- \return Ambient temperature.
- */
-extern float calculate_TA(float, float, int, int);
+class MLX90620
+{
+public:
+  // constructor and destructor
+  MLX90620(int);
+  ~MLX90620();
+  void loop();
+  // get functions
+  byte getEEPROM(int);
+  int getIRDATA(int);
+protected:
+  // calculations
+  void calculateTA();
+  void calculateTO();
+  // configuration/initialization functions
+  void checkConfigReg(int);
+  void config(int);
+  void writeTrimmingValue(byte val);
+  void varInitialization();
+  // read function get from the sensor a specified quantity and store it into the respective variable
+  void readConfigReg();
+  void readCPIX();
+  void readEEPROM();
+  void readIR(int*);
+  void readPTAT();
+  // serial interface
+  void transmitTemperatures();
 
-/**
- \brief Calculate all the 64 pixels temperatures based on the IRDATA values and stores them into the temperatures vector. All the formulas can be found on the datasheet. See page 16 of the datasheet.
- \param  The calculation required parameters
- */
-extern void calculate_TO(int, int, int*, int*, float*, float, float, int, int, int, int*, float*);
-
-/**
- \brief Configure the oscilator frequency (uses config_MLX90620_Hz function) if the configuration register is equal to 0x04.
- \param  Frequency value.
- */
-extern void check_Config_Reg_MLX90620(int);
-
-/**
- \brief Configure the oscilator frequency. The frequency value must be 32, 16, 8, 4, 2 1 or 0 Hz. By default (if it's none of them) the set value is 1.
- \param  Frequency value.
- */
-extern void config_MLX90620_Hz(int);
-
-/**
- \brief Read the MLX90620 configuration refister.
- \return The two register bytes composed as an integer.
- */
-extern int read_Config_Reg_MLX90620();
-
-/**
- \brief Read the CPIX register used to the termal gradient compensation.
- \return The two register bytes composed as an integer.
- */
-extern int read_CPIX_Reg_MLX90620();
-
-/**
- \brief Read the whole EEPROM data and stores it at the parameter vector.
- \param The vector where the data will be stored in.
- */
-extern void read_EEPROM_MLX90620(byte*);
-
-/**
- \brief Read the IR sensors data from the RAM and stores it at the parameter vector.
- \param The vector where the data will be stored in.
-*/
-extern void read_IR(int*);
-
-/**
- \brief Read the PTAT sensor value.
- \return The sensor's value.
- */
-extern unsigned int read_PTAT_Reg_MLX90620();
-
-/**
- \brief Send all the 64 values of the parameter vector by the established serial port (a serial communication must be established).
- \param The 64 entries vector to be sent.
- */
-extern void transmit_temperatures(float*);
-
-/**
- \brief Sets the sensor's trimming value.
- \param  The trimming value.
- */
-extern void write_trimming_value(byte val);
-//#endif // __cplusplus
+private:
+  // Data and configuration parameters
+  byte EEPROM_DATA[64];
+  int IRDATA[64];
+  int CFG, CPIX;
+  unsigned int PTAT;
+  // Temperatures
+  float t_amb;
+  float temperatures[64];
+  // Calculation variables
+  int a_cp, b_cp;          //
+  int a_ij[64];            //
+  int b_ij[64];            //
+  int b_i_scale;           //
+  float emissivity;        //
+  float k_t1, k_t2;        //
+  int v_th, tgc;           //
+  float alpha_ij[64];      //
+  // Refresh frequency
+  int frequency;
+  // auxiliar variable
+  int counter;
+};
+#endif // __cplusplus
